779eaf84d7b00ada291e06b5488e93abefec58c1
Anna Dunets <annie.senchenko@gmail.com>
2023-01-18 16:42:29 +0000

    Parse the PDF

--- file_processing/extract_food.py
+++ file_processing/extract_food.py
@@ -1,32 +1,29 @@
-# importing required modules 
-import PyPDF2
-import re
+import re, pypdf
 
-file = 'sainsburys.pdf'
-our_goods = {}
 
-# creating a pdf file object 
-with open(file, 'rb') as pdfFileObj:
-    # creating a pdf reader object
-    pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+def extract_from_pdf(filename):
+    our_goods = {}
+
+    # creating a pdf file object 
+    with open(filename, 'rb') as pdfFileObj:
+        # creating a pdf reader object
+        myPdfReader = pypdf.PdfReader(pdfFileObj)
     
-    # extracting text from page to var
-    pagesNumber = pdfReader.numPages
-    # creating a page object
-    for i in range(pagesNumber):
-        pageObj = pdfReader.getPage(i)
-        for line in pageObj.extractText().splitlines():
-            pattern = "[0-9]\.[0-9]{1,3}kg"
-            x = re.search(pattern, line)
+        # extracting text from page to var
+        pagesNumber = len(myPdfReader.pages)
+        for i in range(pagesNumber):
+            pageObj = myPdfReader.pages[i]
+            for line in pageObj.extract_text().splitlines():
+                pattern = "[0-9]\.[0-9]{1,3}kg"
+                x = re.search(pattern, line)
             
-            if x:
-                quantity_pattern = "[0-9]\.[0-9]{1,3}kg"
-                quantity = re.findall(quantity_pattern, line)
-                name_pattern = "\s[a-zA-Z\s]+"
-                name = re.findall(name_pattern, line)
-                our_goods[name[0]] = quantity[0]
+                if x:
+                    quantity_pattern = "[0-9]\.[0-9]{1,3}kg"
+                    quantity = re.findall(quantity_pattern, line)
+                    name_pattern = "\s[a-zA-Z\s]+"
+                    name = re.findall(name_pattern, line)
+                    our_goods[name[0]] = quantity[0]
 
-
-for key, value in our_goods.items():
-    print(key)
-    print(value)
+    for key, value in our_goods.items():
+        print(key)
+        print(value)
--- file_processing/receive.py
+++ file_processing/receive.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python
 import sys, os, pika, psycopg2
+from os.path import dirname, abspath
+
 from queue_listener import QueueListener
-import database
+import database, extract_food
+#from pathlib import Path
+
+
 
 def main():
 
@@ -9,8 +14,14 @@ def main():
     listener.listen('some-rabbit', 'received_new_pdf', process_pdf)
 
 def process_pdf(data):
+    
     database.insert_into_db(data['name'])
-    print(" [x] Received %r" % data['name'])
+   # filename = str(Path.cwd()) + '/receipts/' + data['name']
+    filename = str(dirname(dirname(abspath(__file__)))) + '/receipts/' + data['name']
+    print(filename)
+    extract_food.extract_from_pdf(filename)
+    
+
    
 
 if __name__ == '__main__':
